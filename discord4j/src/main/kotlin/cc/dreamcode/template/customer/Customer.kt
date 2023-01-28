15d9cff08789782398b24207778925714f6a3ceb
Ravis96 <ravis96@spoko.pl>
2023-01-28 13:01:12 +0100

    Replace discord4j user uuid to snowflake

--- discord4j/src/main/kotlin/cc/dreamcode/template/customer/Customer.kt
+++ discord4j/src/main/kotlin/cc/dreamcode/template/customer/Customer.kt
@@ -1,17 +1,17 @@
 package cc.dreamcode.template.customer
 
+import discord4j.common.util.Snowflake
 import eu.okaeri.configs.annotation.NameModifier
 import eu.okaeri.configs.annotation.NameStrategy
 import eu.okaeri.configs.annotation.Names
 import eu.okaeri.persistence.document.Document
 import lombok.Data
 import lombok.EqualsAndHashCode
-import java.util.*
 
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Names(strategy = NameStrategy.HYPHEN_CASE, modifier = NameModifier.TO_LOWER_CASE)
 data class Customer(var name: String) : Document() {
-    val uniqueId: UUID
-        get() = path.toUUID()
+    val snowflake: Snowflake
+        get() = Snowflake.of(path.value)
 }
